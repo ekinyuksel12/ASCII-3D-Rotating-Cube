27b8d108a139da17f3cf8f99e2566784086f9fa8
Toprak Ekin Yüksel <ekinyuksel12@gmail.com>
2025-12-06 02:04:55 +0300

    Add per face lighting

--- cube.c
+++ cube.c
@@ -6,6 +6,8 @@
 #define WIDTH 320
 #define HEIGHT 88
 
+const char *luminance = ".,-~:;=!*#$@";
+
 //Character that is used for the background
 int backgroundASCII = ' ';
 float incrementSpeed = 0.6;
@@ -18,6 +20,9 @@ float x,y,z;
 float ooz; //one over z
 int xProjected, yProjected;
 int idx;
+
+float lightX = 0, lightY = -1, lightZ = -1;
+float magnitudeOfLight = sqrt(2);
 
 float cubeWidth = 20;
 int width = WIDTH;
@@ -44,6 +49,28 @@ float calculateZ(float i, float j, float k) {
     return i * (sin(B)) 
         - j * (sin(A) * cos(B))
         + k * (cos(A) * cos(B));
+}
+
+char calculateShade(int normalX, int normalY, int normalZ){
+    float rotatedNormalX = calculateX(normalX, normalY, normalZ);
+    float rotatedNormalY = calculateY(normalX, normalY, normalZ);
+    float rotatedNormalZ = calculateZ(normalX, normalY, normalZ);
+
+    // Dot product of normal vector and light vector tells us how aligned the surface is with the light
+    float lightAllignment = (rotatedNormalX * lightX + rotatedNormalY * lightY + rotatedNormalZ * lightZ);
+
+    // Normalize L by dividing by the magnitude of the light vector. 
+    // Now L is guaranteed to be in the range [-1.0, 1.0]
+    lightAllignment = lightAllignment / magnitudeOfLight;
+
+    // Map the L value to our ASCII palette. L is now in range [0, 11]
+    int index = (int)((lightAllignment + 1.0f) * 5.5f);
+
+    // Clamp the index
+    if (index < 0) index = 0;
+    if (index > 11) index = 11;
+
+    return luminance[index];
 }
 
 void calculatePoints (float cubeX, float cubeY, float cubeZ, int ch){
@@ -74,14 +101,21 @@ int main () {
         memset(screenBuffer, backgroundASCII, width * height);
         memset(zBuffer, 0, width * height * 4);
 
+        char shadeBack  = calculateShade(0, 0, -1);
+        char shadeRight = calculateShade(1, 0, 0);
+        char shadeLeft  = calculateShade(-1, 0, 0);
+        char shadeFront = calculateShade(0, 0, 1);
+        char shadeTop   = calculateShade(0, -1, 0);
+        char shadeBottom= calculateShade(0, 1, 0);
+
         for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed){
             for (float cubeY = -cubeWidth; cubeY < cubeWidth; cubeY += incrementSpeed){
-                calculatePoints(cubeX, cubeY, -cubeWidth, '@');
-                calculatePoints(cubeWidth, cubeY, cubeX, '$');
-                calculatePoints(-cubeWidth, cubeY, -cubeX, '~');
-                calculatePoints(-cubeX, cubeY, cubeWidth, '#');
-                calculatePoints(cubeX, -cubeWidth, -cubeY, ';');
-                calculatePoints(cubeX, cubeWidth, cubeY, '+');
+                calculatePoints(cubeX, cubeY, -cubeWidth, shadeBack);
+                calculatePoints(cubeWidth, cubeY, cubeX, shadeRight);
+                calculatePoints(-cubeWidth, cubeY, -cubeX, shadeLeft);
+                calculatePoints(-cubeX, cubeY, cubeWidth, shadeFront);
+                calculatePoints(cubeX, -cubeWidth, -cubeY, shadeTop);
+                calculatePoints(cubeX, cubeWidth, cubeY, shadeBottom);
             }
         }
 
